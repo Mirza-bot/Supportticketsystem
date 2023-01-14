1afd3a9a646190f31526f44479b519fc702bfd76
Malkoc Mirza <81041253+Mirza-bot@users.noreply.github.com>
2023-01-14 14:29:40 +0100

    ReduxTK => added authSlice with register/logout action

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -9,6 +11,12 @@ function Register() {
   });
 
   const { email, password } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((previousState) => ({
@@ -19,6 +27,13 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
 
   return (
